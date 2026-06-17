9fa6436d362067f855353739ca95f6dcbf39fed3
Anjali Gupta <anjali.gupta.as15@gmail.com>
2026-06-17 22:19:39 +0530

    17-06-2026 : Pushed code for Early Loading Singleton

---
+++ lld/src/class_07/earlyLoading/Main.java
@@ -0,0 +1,37 @@
+package class_07.earlyLoading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        // Regular
+        Singleton singleton1 = Singleton.getInstance(); // Singleton constructor
+        Singleton singleton2 = Singleton.getInstance();
+        singleton1.print();
+        singleton2.print();
+        System.out.println();
+
+
+        // Multithreaded environment
+        //ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        // Thread 1
+        Runnable task1 = () -> {
+            Singleton singleton3 = Singleton.getInstance();
+            singleton3.print();
+        };
+
+        // Thread 2
+        Runnable task2 = () -> {
+            Singleton singleton4 = Singleton.getInstance();
+            singleton4.print();
+        };
+
+        executorService.execute(task1);
+        executorService.execute(task2);
+        executorService.shutdown();
+    }
+}
+
---
+++ lld/src/class_07/earlyLoading/Singleton.java
@@ -0,0 +1,17 @@
+package class_07.earlyLoading;
+
+public class Singleton {
+    private Singleton() {
+        System.out.println("Singleton constructor");
+    }
+
+    private static Singleton instance = new Singleton();    // Singleton constructor
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public static void print() {
+        System.out.println("Print Method");
+    }
+}
